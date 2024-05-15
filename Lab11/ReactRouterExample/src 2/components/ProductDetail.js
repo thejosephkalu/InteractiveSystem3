0adf088853a5cf6247f3924cdfcf0d30b5d2a38b
thejosephkalu <kalu@tlu.ee>
2024-05-15 11:22:13 +0300

    updated

--- Lab11/ReactRouterExample/src 2/components/ProductDetail.js
+++
@@ -1,5 +0,0 @@
-export const ProductDetail = () => {
-  return (
-    <div>ProductDetail</div>
-  )
-}
--- Lab11/ReactRouterExample/src 2/components/ProductList.js
+++
@@ -1,5 +0,0 @@
-export const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
